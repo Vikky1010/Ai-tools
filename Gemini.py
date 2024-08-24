214ecc7175187495c7e7a8818bb88404aae15bbf
Vikky1010 <vikramkelavalasa10@gmail.com>
2024-08-24 15:30:52 +0530

    Add files via upload

---
+++ Gemini.py
@@ -0,0 +1,18 @@
+import os
+import google.generativeai as genai
+import streamlit as st
+os.environ["GOOGLE_API_KEY"]="AIzaSyAa2BmndQuZnM_MSjfuJZstskb0xZBnLJ8"
+#configure
+genai.configure(api_key = os.environ["GOOGLE_API_KEY"])
+
+
+model = genai.GenerativeModel("gemini-1.5-pro")
+
+def generate(input_text):
+    response = model.generate_content(input_text)
+    return response.text
+
+st.title("Gemini gpt")
+input_text = st.text_input("Write your prompt")
+if input_text:
+    st.write(generate(input_text))
